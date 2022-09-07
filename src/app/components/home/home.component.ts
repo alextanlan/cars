a7cd97155bc1ee2049f4cc7a6e6e013f81ee5e7f
Aleksei Maratkanov <alextanlan@gmail.com>
2022-09-08 00:25:50 +0200

    Add dashboard link

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {AuthService} from "@auth0/auth0-angular";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class HomeComponent {
+  constructor(public auth: AuthService) {
   }
-
 }
