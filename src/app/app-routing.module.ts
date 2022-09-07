57ef2cc5eb524b9831a5b7d2a63b244e683e229e
Aleksei Maratkanov <alextanlan@gmail.com>
2022-09-07 23:33:54 +0200

    Add guard for dashboard route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 import {HomeComponent} from "./components/home/home.component";
+import {AuthGuard} from "@auth0/auth0-angular";
+
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   {
     path: 'dashboard',
-    loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
+    loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule),
+    canLoad: [AuthGuard]
   },
   { path: '**', redirectTo: ''} // TODO: create "Not Found" page
 ];
