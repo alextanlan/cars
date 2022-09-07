d1bb06a7faafda6e390afe429f2fa8a640a5a0b9
Aleksei Maratkanov <alextanlan@gmail.com>
2022-09-07 21:40:34 +0200

    Update routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {AppComponent} from "./app.component";
 
 const routes: Routes = [
+  { path: '', component: AppComponent },
   {
     path: 'dashboard',
     loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
-  }
+  },
+  { path: '**', redirectTo: ''} // TODO: create "Not Found" page
 ];
 
 @NgModule({
