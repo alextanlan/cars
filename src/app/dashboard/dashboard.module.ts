41c2329456f353146c829cf0179ebf24b9bcfd90
Aleksei Maratkanov <alextanlan@gmail.com>
2022-09-07 23:34:52 +0200

    Add basic layouts for cars-list and car-card

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
+import { CarsListComponent } from './components/cars-list/cars-list.component';
+import { CarCardComponent } from './components/car-card/car-card.component';
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    CarsListComponent,
+    CarCardComponent
   ],
   imports: [
     CommonModule,
